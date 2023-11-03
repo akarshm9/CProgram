f9fa3d8b02342cc459dba13f05f4d34d84547bd0
akarshm9 <134285546+akarshm9@users.noreply.github.com>
2023-11-03 15:49:09 +0530

    Add files via upload

---
+++ CTut3-ArithmeticOps.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+
+	printf("Operators in C programming");
+	
+	/*Operators are some specific symbols which do a specific task when used
+It alsways exist between two variables or numbers called as operands. There are 
+many kinds of operators they are
+1.ARITHMETIC OPERATORS -> +,-,*,/,%
+2.LOGICAL OPERATORS    ->&&,||, !
+3.RELATIONAL OPERATORS
+4.BITWISE OPERATORS
+5.LOGICAL SHIFT LEFT
+6.LOGICAL SHIFT RIGHT
+	*/
+	
+	int a=10,b=5,c=5;
+	//Arithmetic operators
+	printf("\nSum is - %d+%d=%d\n",a,b,a+b);
+	printf("Difference is %d-%d=%d\n",a,b,a-b);
+	printf("Product is %d*%d=%d\n",a,b,a*b);
+	printf("Quotient is %d/%d=%d\n",a,b,a/b);
+	printf("Remainder is %d@%d=%d\n",a,b,a%b);
+
+
+
+
+return 0;}
+
---
+++ CTut3-LogicalOps.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+	printf("Logical operators in C\n");
+		/*Operators are some specific symbols which do a specific task when used
+It alsways exist between two variables or numbers called as operands. There are
+many kinds of operators they are
+1.ARITHMETIC OPERATORS -> +,-,*,/,%
+2.LOGICAL OPERATORS    ->&&,||, !
+3.RELATIONAL OPERATORS
+4.BITWISE OPERATORS
+5.LOGICAL SHIFT LEFT
+6.LOGICAL SHIFT RIGHT
+	*/
+
+	int a=10,b=5,c=5;
+	
+
+
+
+
+return 0;}
+
