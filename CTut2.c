10940716f5990d7ef03f122c6943a217bec3b0e9
akarshm9 <134285546+akarshm9@users.noreply.github.com>
2023-11-03 15:06:25 +0530

    Create CTut2.c
    File contains code about variables

---
+++ CTut2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+
+	printf("Variables in C programming-\n\n");
+	/* Variables are such tokens in programming which are used to store value of
+	 which are given by user. Simply we can say that variables are containsrs
+	 to store values.  We create variables in C using the following rule-
+
+	 first of all write the data of the variable then write name then you
+	 can assign value. See syntax and example
+<data_type> name = value;
+Example int a=7;,char b='h'; and more
+	 */
+	 
+	 int a=66;
+	 char grade='A';
+	 float marks=77.32;
+	 printf("Printing value of a-%d\n",a);
+	 
+	 printf("Printing value of grade- %c\n",grade);
+	 
+	 printf("Printing value of status-%f",marks);
+
+
+
+
+return 0;}
